6bfab96c3b0d0bada72f227c9c9bd791fe5c0f0e
vovchik999 <vovchiksemchik@gmail.com>
2014-04-03 23:51:49 +0300

    Test comment

--- GitTest/src/com/vovchik/git/test/GitTest.java
+++ GitTest/src/com/vovchik/git/test/GitTest.java
@@ -3,5 +3,6 @@ package com.vovchik.git.test;
 public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is fun");
+	    System.out.println("Git is fun!!!!");
 	  }
 }
